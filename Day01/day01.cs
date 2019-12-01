b9de1aab9eecb29ee7e500c32b343c65ae2f8df5
Floris Douw <fdouw@users.noreply.github.com>
2019-12-01 16:04:34 +0100

    Add part 2

--- Day01/day01.cs
+++ Day01/day01.cs
@@ -15,17 +15,38 @@ namespace AoC2019
             }
 
             Console.WriteLine("Day 01");
+
+            // Part 01
             using (StreamReader reader = new StreamReader(inputPath))
             {
-                long sum = 0;
+                long fuel = 0;
                 string line;
                 // Read lines 'by hand', because ReadToEnd().Split("\n") adds an entry that breaks
                 // Linq
                 while (null != (line = reader.ReadLine()))
                 {
-                    sum += Int32.Parse(line) / 3 - 2;
+                    fuel += Int32.Parse(line) / 3 - 2;
                 }
-                Console.WriteLine($"1. Total Fuel: {sum}");
+                Console.WriteLine($"1. Total Fuel: {fuel}");
+            }
+
+            // Part 2
+            using (StreamReader reader = new StreamReader(inputPath))
+            {
+                long fuel, totalFuel = 0;
+                string line;
+                // Read lines 'by hand', because ReadToEnd().Split("\n") adds an entry that breaks
+                // Linq
+                while (null != (line = reader.ReadLine()))
+                {
+                    fuel = Int32.Parse(line) / 3 - 2;
+                    totalFuel += fuel;
+                    while (0 < (fuel = fuel / 3 - 2))
+                    {
+                        totalFuel += fuel;
+                    }
+                }
+                Console.WriteLine($"2. Total Fuel: {totalFuel}");
             }
         }
     }

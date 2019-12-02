e9809236641ea3b6cbe714444a198fa5dcec7b0f
Floris Douw <fdouw@users.noreply.github.com>
2019-12-02 20:12:30 +0100

    Refactor C# solution to day 2

--- Day02/Day02.cs
+++ Day02/Day02.cs
@@ -27,63 +27,52 @@ namespace AoC2019
             {
                 // All input is on the first line
                 string line = sr.ReadLine();
-                int[] codes = line.Split(',').Select(x => Int32.Parse(x)).ToArray();
+                int[] data = line.Split(',').Select(x => Int32.Parse(x)).ToArray();
+                int[] codes = new int[data.Length];
 
-                // Restore 1202 state
-                codes[1] = 12;
-                codes[2] = 02;
-
-                // Execute intcode
-                int pos = 0;
-                while (codes[pos] != OpCode.END)
-                {
-                    if (codes[pos] == OpCode.ADD)
-                    {
-                        codes[codes[pos + 3]] = codes[codes[pos + 1]] + codes[codes[pos + 2]];
-                    }
-                    else if (codes[pos] == OpCode.MUL)
-                    {
-                        codes[codes[pos + 3]] = codes[codes[pos + 1]] * codes[codes[pos + 2]];
-                    }
-                    pos += 4;
-                }
-                Console.WriteLine($"1. code: {codes[0]}");
+                // Part 1
+                Console.WriteLine($"1. code: {execute(12, 02)}");
 
                 // Part 2
-                int[] data = line.Split(',').Select(x => Int32.Parse(x)).ToArray();
-
                 for (int noun = 0; noun < 100; noun++)
                 {
                     for (int verb = 0; verb < 100; verb++)
                     {
-                        // Copy orignal array data
-                        for (int i = 0; i < data.Length; i++) codes[i] = data[i];
-
-                        codes[1] = noun;
-                        codes[2] = verb;
-
-                        // Execute intcode
-                        pos = 0;
-                        while (codes[pos] != OpCode.END)
-                        {
-                            if (codes[pos] == OpCode.ADD)
-                            {
-                                codes[codes[pos + 3]] = codes[codes[pos + 1]] + codes[codes[pos + 2]];
-                            }
-                            else if (codes[pos] == OpCode.MUL)
-                            {
-                                codes[codes[pos + 3]] = codes[codes[pos + 1]] * codes[codes[pos + 2]];
-                            }
-                            pos += 4;
-                        }
-
                         // Check if we have found the output we're asked for
-                        if (codes[0] == 19690720)
+                        if (execute(noun, verb) == 19690720)
                         {
                             Console.WriteLine($"2. input: {100 * noun + verb}");
                             return;
                         }
                     }
+                }
+
+                // Function to compute the intcode
+                int execute (int noun, int verb)
+                {
+                    // Copy orignal array data
+                    for (int i = 0; i < data.Length; i++) codes[i] = data[i];
+
+                    codes[1] = noun;
+                    codes[2] = verb;
+
+                    // Execute intcode
+                    int pos = 0;
+                    while (codes[pos] != OpCode.END)
+                    {
+                        if (codes[pos] == OpCode.ADD)
+                        {
+                            codes[codes[pos + 3]] = codes[codes[pos + 1]] + codes[codes[pos + 2]];
+                        }
+                        else if (codes[pos] == OpCode.MUL)
+                        {
+                            codes[codes[pos + 3]] = codes[codes[pos + 1]] * codes[codes[pos + 2]];
+                        }
+                        pos += 4;
+                    }
+
+                    // Result is in address 0
+                    return codes[0];
                 }
             }
         }

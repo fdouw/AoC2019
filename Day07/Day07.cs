187d853973ba466b99e7e264ce881f2227babb9b
Floris Douw <fdouw@users.noreply.github.com>
2019-12-07 19:34:15 +0100

    Add C# solution to part 2

--- Day07/Day07.cs
+++ Day07/Day07.cs
@@ -13,35 +13,43 @@ namespace AoC2019
 
             // Read input and create IntCode instances
             string filename = @"input";
-            IntCode amplifier;
+            IntCodeMachine amp;
+            IntCodeMachine[] amplifier;
             using (StreamReader sr = new StreamReader(filename))
             {
                 // All data is on the first line
                 int[] data = sr.ReadLine().Trim().Split(",").Select(s => Int32.Parse(s)).ToArray();
+
                 // Only 1 IntCode is needed, as they all use the same instructions, and they reset
                 // their state between runs anyway.  Therefor we can reuse the amplifier.
-                amplifier = new IntCode(data);
+                amp = new IntCodeMachine("Part1-test", data);
+
+                // For part 2, we do need distinct amplifiers
+                amplifier = new IntCodeMachine[N_AMP];
+                for (int i = 0; i < N_AMP; i++) amplifier[i] = new IntCodeMachine(i.ToString(), data);
             }
+
+            System.Console.WriteLine("Day 07");
 
             int[] output = new int[N_AMP];
             int thrusterOutput = 0;
             for (int a = 0; a < N_AMP; a++)
             {
-                output[0] = amplifier.Run(new int[] {a, 0}).Last();
+                output[0] = amp.Run(new int[] {a, 0}, true);
                 for (int b = 0; b < N_AMP; b++)
                 {
                     if (b == a) continue;
-                    output[1] = amplifier.Run(new int[] {b, output[0]}).Last();
+                    output[1] = amp.Run(new int[] {b, output[0]}, true);
                     for (int c = 0; c < N_AMP; c++)
                     {
                         if (c == a || c == b) continue;
-                        output[2] = amplifier.Run(new int[] {c, output[1]}).Last();
+                        output[2] = amp.Run(new int[] {c, output[1]}, true);
                         for (int d = 0; d < N_AMP; d++)
                         {
                             if (d == a || d == b || d == c) continue;
-                            output[3] = amplifier.Run(new int[] {d, output[2]}).Last();
+                            output[3] = amp.Run(new int[] {d, output[2]}, true);
                             int e = 10 - a - b - c - d; // No loop needed!
-                            output[4] = amplifier.Run(new int[] {e, output[3]}).Last();
+                            output[4] = amp.Run(new int[] {e, output[3]}, true);
                             if (output[4] > thrusterOutput)
                             {
                                 thrusterOutput = output[4];
@@ -50,13 +58,65 @@ namespace AoC2019
                     }
                 }
             }
-
-            System.Console.WriteLine("Day 07");
             System.Console.WriteLine($"1. {thrusterOutput}");
+
+            // Part 2
+            thrusterOutput = 0;
+            int intermediateResult = 0;
+            int[] phase, bestPhase = null;
+            for (int a = 5; a < 10; a++)
+            {
+                for (int b = 5; b < 10; b++)
+                {
+                    if (b == a) continue;
+                    for (int c = 5; c < 10; c++)
+                    {
+                        if (c == a || c == b) continue;
+                        for (int d = 5; d < 10; d++)
+                        {
+                            if (d == a || d == b || d == c) continue;
+                            int e = 35 - a - b - c - d; // No loop needed!
+
+                            // Starting the machines also resets their state
+                            phase = new int[] { a, b, c, d, e };
+                            amplifier[0].Start(new int[] {phase[0], 0}, out output[0]);
+                            amplifier[1].Start(new int[] {phase[1], output[0]}, out output[1]);
+                            amplifier[2].Start(new int[] {phase[2], output[1]}, out output[2]);
+                            amplifier[3].Start(new int[] {phase[3], output[2]}, out output[3]);
+                            if (amplifier[4].Start(new int[] {phase[4], output[3]}, out output[4]))
+                            {
+                                intermediateResult = output[4];
+                            }
+                            while (amplifier[0].active)
+                            {
+                                amplifier[0].Next(new int[] {output[4]}, out output[0]);
+                                amplifier[1].Next(new int[] {output[0]}, out output[1]);
+                                amplifier[2].Next(new int[] {output[1]}, out output[2]);
+                                amplifier[3].Next(new int[] {output[2]}, out output[3]);
+
+                                // output is always overwritten, even if the machine has no output
+                                // Next() return true iff the machine actually has output (and the
+                                // value is meaningful)
+                                if (amplifier[4].Next(new int[] {output[3]}, out output[4]))
+                                {
+                                    intermediateResult = output[4];
+                                }
+                            }
+
+                            if (intermediateResult > thrusterOutput)
+                            {
+                                thrusterOutput = intermediateResult;
+                                bestPhase = phase;
+                            }
+                        }
+                    }
+                }
+            }
+            System.Console.WriteLine($"2. {thrusterOutput} [{bestPhase[0]},{bestPhase[1]},{bestPhase[2]},{bestPhase[3]},{bestPhase[4]}]");
         }
     }
 
-    class IntCode
+    class IntCodeMachine
     {
         public const int OP_ADD = 1;   // Add
         public const int OP_MUL = 2;   // Multiply
@@ -71,28 +131,62 @@ namespace AoC2019
         public const int MOD_POS = 0;
         public const int MOD_VAL = 1;
 
-        private int[] data;
+        public string name { get; }     // Identifier for the machine instance
+        private int[] data;             // Initial state, keep to reset
+        private int[] code;             // Current state
+        private int idx;                // Execution pointer
+        
+        public bool active { get; private set; }   // Is the machine currently running
 
-        public IntCode (int[] data)
+        public IntCodeMachine (string name, int[] data)
         {
+            this.name = name;
             this.data = new int[data.Length];
+            this.code = new int[data.Length];
             for (int i = 0; i < data.Length; i++) this.data[i] = data[i];
         }
 
-        public IEnumerable<int> Run(int[] inputs, bool verbose = false)
+        public int Run (int[] input, bool force = false)
         {
-            // First copy the data (code) so as not to mess up the original
-            int[] code = new int[data.Length];
-            for (int i = 0; i < data.Length; i++) code[i] = data[i];
+            if (active && !force) throw new Exception($"Machine [{name}] already active");
 
-            int ptr = 0;    // current position in the program
+            int output;
+            Start(input, out output);   // Assume the machine has no intermediate output!
+            return output;
+        }
+
+        public void Reset()
+        {
+            for (int i = 0; i < data.Length; i++) code[i] = data[i];
+            idx = 0;
+            active = true;
+        }
+
+        public void Stop() =>  active = false;
+
+        public bool Start (int[] input, out int output)
+        {
+            Reset();
+            return Next(input, out output);
+        }
+
+        public bool Next(int[] inputs, out int output)
+        {
+            // Machine must have been initialised
+            if (!active) throw new Exception("Machine not active");
+            
             int in_ptr = 0; // current position in the input array
+
+            // System.Console.WriteLine($"[{name}][IN] idx = {idx}");
+
+            // Guarantee that we have output
+            output = Int32.MaxValue;
 
             bool running = true;
             int a, b, c;
             while (running)
             {
-                int cmd = code[ptr++];
+                int cmd = code[idx++];
                 int opcode = cmd % 100;
                 int mode1 = (cmd / 100) % 10;
                 int mode2 = (cmd / 1000) % 10;
@@ -100,64 +194,78 @@ namespace AoC2019
                 switch (opcode)
                 {
                     case OP_ADD:
-                        if (verbose) System.Console.WriteLine($"> ADD {ptr} {mode1} {mode2}");
-                        a = (mode1 == MOD_POS) ? code[code[ptr++]] : code[ptr++];
-                        b = (mode2 == MOD_POS) ? code[code[ptr++]] : code[ptr++];
-                        c = code[ptr++];    // Write address: never in immediate mode
+                        // Console.WriteLine("OP_ADD");
+                        a = (mode1 == MOD_POS) ? code[code[idx++]] : code[idx++];
+                        b = (mode2 == MOD_POS) ? code[code[idx++]] : code[idx++];
+                        c = code[idx++];    // Write address: never in immediate mode
                         code[c] = a + b;
                         break;
                     case OP_MUL:
-                        if (verbose) System.Console.WriteLine($"> MUL {ptr} {mode1} {mode2}");
-                        a = (mode1 == MOD_POS) ? code[code[ptr++]] : code[ptr++];
-                        b = (mode2 == MOD_POS) ? code[code[ptr++]] : code[ptr++];
-                        c = code[ptr++];    // Write address: never in immediate mode
+                        // Console.WriteLine("OP_MUL");
+                        a = (mode1 == MOD_POS) ? code[code[idx++]] : code[idx++];
+                        b = (mode2 == MOD_POS) ? code[code[idx++]] : code[idx++];
+                        c = code[idx++];    // Write address: never in immediate mode
                         code[c] = a * b;
                         break;
                     case OP_IN:
-                        if (verbose) System.Console.WriteLine($"> IN {ptr} {mode1}");
+                        // Console.WriteLine("OP_IN");
                         a = inputs[in_ptr++];   // Assume enough inputs
-                        b = code[ptr++];    // Write address: never in immediate mode
+                        b = code[idx++];    // Write address: never in immediate mode
                         code[b] = a;
                         break;
                     case OP_OUT:
-                        if (verbose) System.Console.WriteLine($"> OUT {ptr} {mode1}");
-                        yield return (mode1 == MOD_POS) ? code[code[ptr++]] : code[ptr++];
-                        break;
+                        // Console.WriteLine("OP_OUT");
+                        output = (mode1 == MOD_POS) ? code[code[idx++]] : code[idx++];
+                        // System.Console.WriteLine($"[{name}][OUT] idx = {idx}, val = {output}");
+                        return true;
                     case OP_JMP_T:
-                        if (verbose) System.Console.WriteLine($"> JMP TRUE {ptr} {mode1}");
-                        a = (mode1 == MOD_POS) ? code[code[ptr++]] : code[ptr++];
-                        b = (mode2 == MOD_POS) ? code[code[ptr++]] : code[ptr++];
-                        if (a != 0) ptr = b;
+                        // Console.WriteLine("OP_JMP_T");
+                        a = (mode1 == MOD_POS) ? code[code[idx++]] : code[idx++];
+                        b = (mode2 == MOD_POS) ? code[code[idx++]] : code[idx++];
+                        if (a != 0)
+                        {
+                            idx = b;
+                            // Console.WriteLine($"Jump on True to: {idx}");
+                        }
                         break;
                     case OP_JMP_F:
-                        if (verbose) System.Console.WriteLine($"> JMP FALSE {ptr} {mode1}");
-                        a = (mode1 == MOD_POS) ? code[code[ptr++]] : code[ptr++];
-                        b = (mode2 == MOD_POS) ? code[code[ptr++]] : code[ptr++];
-                        if (a == 0) ptr = b;
+                        // Console.WriteLine("OP_JMP_F");
+                        a = (mode1 == MOD_POS) ? code[code[idx++]] : code[idx++];
+                        b = (mode2 == MOD_POS) ? code[code[idx++]] : code[idx++];
+                        if (a == 0)
+                        {
+                            idx = b;
+                            // Console.WriteLine($"Jump on False to: {idx}");
+                        }
                         break;
                     case OP_LT:
-                        if (verbose) System.Console.WriteLine($"> LESS THAN {ptr} {mode1}");
-                        a = (mode1 == MOD_POS) ? code[code[ptr++]] : code[ptr++];
-                        b = (mode2 == MOD_POS) ? code[code[ptr++]] : code[ptr++];
-                        c = code[ptr++];    // Write address: never in immediate mode
+                        // Console.WriteLine("OP_LT");
+                        a = (mode1 == MOD_POS) ? code[code[idx++]] : code[idx++];
+                        b = (mode2 == MOD_POS) ? code[code[idx++]] : code[idx++];
+                        c = code[idx++];    // Write address: never in immediate mode
                         code[c] = (a < b) ? 1 : 0;
                         break;
                     case OP_EQ:
-                        if (verbose) System.Console.WriteLine($"> EQUAL {ptr} {mode1}");
-                        a = (mode1 == MOD_POS) ? code[code[ptr++]] : code[ptr++];
-                        b = (mode2 == MOD_POS) ? code[code[ptr++]] : code[ptr++];
-                        c = code[ptr++];    // Write address: never in immediate mode
+                        // Console.WriteLine("OP_EQ");
+                        a = (mode1 == MOD_POS) ? code[code[idx++]] : code[idx++];
+                        b = (mode2 == MOD_POS) ? code[code[idx++]] : code[idx++];
+                        c = code[idx++];    // Write address: never in immediate mode
                         code[c] = (a == b) ? 1 : 0;
                         break;
                     case OP_END:
-                        running = false;
-                        break;
+                        // Console.WriteLine("OP_END");
+                        // System.Console.WriteLine($"[{name}][END] idx = {idx}");
+                        active = false;
+                        return false;
                     default:
-                        System.Console.WriteLine($"Unknown OpCode: {opcode}");
-                        running = false;
-                        break;
+                        active = false;
+                        throw new Exception($"Unknown OpCode: {opcode}");
                 }
             }
+
+            // End of run, but no output set (apparently)
+            active = false;
+            return false;
         }
     }
 }

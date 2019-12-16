b1c9f6c82692a4aa16fc42cef013bee986b52b46
Floris Douw <fdouw@users.noreply.github.com>
2019-12-16 22:39:06 +0100

    Rewrite to solution for only part 2

--- Day16/Day16.cs
+++ Day16/Day16.cs
@@ -12,11 +12,25 @@ namespace AoC2019
 
             // Read the data
             string filename = "input";
+            // filename = "input-ex-2-84462026";
             int[] data;
+            int offset;
             using (StreamReader sr = new StreamReader(filename))
             {
-                data = sr.ReadLine().Trim().ToCharArray().Select(c => Int32.Parse(c.ToString())).ToArray();
+                string rawString = sr.ReadLine().Trim();
+                offset = Int32.Parse(rawString.Substring(0,7)); // % rawString.Length;
+                // int targetLength = (10_000 * rawString.Length - offset) / rawString.Length + 1;
+                // offset %= rawString.Length;
+                int[] raw = rawString.ToCharArray().Select(c => Int32.Parse(c.ToString())).ToArray();
+                data = new int[raw.Length * 10_000];
+                for (int i = 0; i < data.Length; i++)
+                {
+                    data[i] = raw[i % raw.Length];
+                }
             }
+
+            // System.Console.WriteLine(offset);
+            // System.Console.WriteLine(data.Length);
 
             // Cache the computation for individual digits
             int[,] transform = new int[10,3];
@@ -28,26 +42,38 @@ namespace AoC2019
             int[] modifier = new int[]{ 1, 2, 1, 0 };   // Shift by 1 to use these as indices for the transform matrix
 
             int[] newData = new int[data.Length];
+            int[] tmp;
             for (int phase = 0; phase < 100; phase++)
             {
-                for (int d = 0; d < data.Length; d++)   // Loop for the digit we are computing
+                int d;
+                for (d = offset; d < data.Length / 2; d++)   // Loop for the digit we are computing
                 {
                     int m = 0;
-                    for (int i = 0; i < data.Length; i++)   // Loop for the digit used in the computation
+                    for (int i = d; i < data.Length; i++)   // Loop for the digit used in the computation
                     {
                         // Update the modifier according to the digit we're computing
                         if ((i + 1) % (d + 1) == 0)
                         {
-                            m = (m + 1) % 4;                    
+                            m = (m == 3) ? 0 : m + 1;
                         }
                         newData[d] += transform[data[i], modifier[m]];
                     }
-                    newData[d] = Math.Abs(newData[d] % 10);
+                    newData[d] = (newData[d] < 0) ? -newData[d] % 10 : newData[d] % 10;
                 }
-                System.Console.WriteLine($"{phase,3}: {String.Concat(newData).Substring(0,8)}");
+                int cur = 0;
+                for (int e = data.Length - 1; e >= d; e--)
+                {
+                    cur += data[e];
+                    newData[e] = (cur < 0) ? (-cur % 10) : cur % 10;   // Second half is all multiplied by 1
+                }
+                tmp = data;
                 data = newData;
-                newData = new int[data.Length];
+                newData = tmp;
+                Array.Clear(newData, offset, newData.Length - offset);
             }
+            // System.Console.WriteLine($"1. {String.Concat(data).Substring(data.Length-8)}");
+            // System.Console.WriteLine($"1. {String.Concat(data).Substring(0,8)}");
+            System.Console.WriteLine($"1. {String.Concat(data).Substring(offset,8)}");
         }
     }
 }

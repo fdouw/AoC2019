8b98a983a75bf5e5a11fc4bac1457304594e9a33
Floris Douw <fdouw@users.noreply.github.com>
2019-12-22 14:20:47 +0100

    Add LightweightDeck, which makes for faster processing

--- Day22/Day22.cs
+++ Day22/Day22.cs
@@ -11,61 +11,77 @@ namespace AoC2019
         {
             System.Console.WriteLine("Day 22");
 
-            Deck cards = new Deck();
+            string[] shuffle;
             string filename = "input";
             using (StreamReader sr = new StreamReader(filename))
             {
-                string line;
-                while ((line = sr.ReadLine()) != null)
+                shuffle = sr.ReadToEnd().Trim().Split("\n").ToArray();
+            }
+
+            // Part 1
+            // Deck cards = new Deck(10_007);
+            // ShuffleDeck(cards, shuffle);
+            // System.Console.WriteLine($"1. {cards.IndexOf(2019)}");
+
+            LightweightDeck lwCards = new LightweightDeck(10_007, 2019);
+            ShuffleDeck(lwCards, shuffle);
+            System.Console.WriteLine($"1b {lwCards.Position}");
+
+            // Part 2
+            // cards = new Deck(119315717514047);
+            // ShuffleDeck(cards, shuffle, 101741582076661);
+            // System.Console.WriteLine($"2. {cards.ItemAt(2020)}");
+        }
+
+        private static void ShuffleDeck (IDeck cards, string[] shuffle, long times = 1)
+        {
+            for (int i = 0; i < times; i++)
+            {
+                foreach(string line in shuffle)
                 {
                     if (line.StartsWith("deal with increment"))
                     {
-                        System.Console.WriteLine($"{Int32.Parse(line.Substring(20))}");
-                        cards.DealIncrement(Int32.Parse(line.Substring(20)));
+                        cards.DealIncrement(Int64.Parse(line.Substring(20)));
                     }
                     else if (line == "deal into new stack")
                     {
-                        System.Console.WriteLine($"new stack");
                         cards.Reverse();
                     }
                     else if (line.StartsWith("cut"))
                     {
-                        System.Console.WriteLine($"{Int32.Parse(line.Substring(4))}");
-                        cards.Cut(Int32.Parse(line.Substring(4)));
+                        cards.Cut(Int64.Parse(line.Substring(4)));
                     }
                 }
-                System.Console.WriteLine($"1. {cards.IndexOf(2019)}");
-                // System.Console.WriteLine($"{cards.ToString()}");
             }
         }
     }
 
-    class Deck
+    class Deck : IDeck
     {
-        internal int Size { get; }
-        internal int Last { get => Size - 1; }
+        internal long Size { get; }
+        internal long Last { get => Size - 1; }
 
-        private int[] cards;
+        private long[] cards;
 
-        internal Deck (int s = 10_007)
+        internal Deck (long s)
         {
             Size = s;
-            cards = new int[Size];
-            for (int i = 0; i < Size; i++)
+            cards = new long[Size];
+            for (long i = 0; i < Size; i++)
                 cards[i] = i;
         }
 
-        internal void Reverse ()
+        public void Reverse ()
         {
-            for (int i = 0; i < Size / 2; i++)
+            for (long i = 0; i < Size / 2; i++)
             {
-                int tmp = cards[i];
+                long tmp = cards[i];
                 cards[i] = cards[Last - i];
                 cards[Last - i] = tmp;
             }
         }
 
-        internal void Cut (int n)
+        public void Cut (long n)
         {
             if (n < 0)
             {
@@ -76,35 +92,70 @@ namespace AoC2019
             }
             else
             {
-                int[] tmp = new int[Math.Abs(n)];
-                for (int i = 0; i < n; i++)
+                long[] tmp = new long[Math.Abs(n)];
+                for (long i = 0; i < n; i++)
                     tmp[i] = cards[i];
-                for (int i = n; i < Size; i++)
+                for (long i = n; i < Size; i++)
                     cards[i-n] = cards[i];
-                for (int i = 0; i < n; i++)
+                for (long i = 0; i < n; i++)
                     cards[Size - n + i] = tmp[i];
             }
         }
 
-        internal void DealIncrement (int n)
+        public void DealIncrement (long n)
         {
-            int[] tmp = new int[Size];
-            for (int i = 0; i < Size; i++)
+            long[] tmp = new long[Size];
+            for (long i = 0; i < Size; i++)
                 tmp[i * n % Size] = cards[i];   // Probably more efficient to have an extra j += n
             cards = tmp;
         }
 
-        internal int IndexOf (int item)
+        internal long IndexOf (long item)
         {
-            for (int i = 0; i < Size; i++)
+            for (long i = 0; i < Size; i++)
                 if (cards[i] == item)
                     return i;
             return -1;
         }
 
-        internal string ToString ()
+        internal long ItemAt (long index) => cards[index];
+    }
+
+    // Instead of simulating an entire deck, follow the position of a single card
+    class LightweightDeck : IDeck
+    {
+        internal long Size { get; }
+        internal long Last { get => Size - 1; }
+        internal long Card { get; }
+        internal long Position { get; private set; }
+
+        internal LightweightDeck (long size, long target)
         {
-            return String.Join(' ', cards);
+            this.Size = size;
+            this.Card = target;
+            this.Position = Card;
         }
+
+        public void Reverse ()
+        {
+            Position = Last - Position;
+        }
+
+        public void Cut (long n)
+        {
+            Position = (Position - n + Size) % Size;
+        }
+
+        public void DealIncrement (long n)
+        {
+            Position = Position * n % Size;
+        }
+    }
+
+    interface IDeck
+    {
+        void Reverse();
+        void Cut(long n);
+        void DealIncrement(long n);
     }
 }
